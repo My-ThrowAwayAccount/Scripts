6d986ae23897dcae2b416094809a175920786399
My-ThrowAwayAccount <87697667+My-ThrowAwayAccount@users.noreply.github.com>
2021-07-21 07:54:20 +0200

    Update TweaksBackup.sh
    + Added Emojis
    + Added a message when the program closes because of not agreeing

--- TweaksBackup/TweaksBackup.sh
+++ TweaksBackup/TweaksBackup.sh
@@ -9,7 +9,7 @@ red='\033[0;31m'
 
 printf "${green}Welcome to ${yellow}TweaksBackup${clear}!\n"
 printf "This script can be used to, you guessed it, back up your tweaks! (well, packages)\n"
-printf "${red}Warning! Please read below with caution.${clear}\n"
+printf "⚠️ ${red}Warning! Please read below with caution.${clear} ⚠️\n"
 printf "This script is ${red}NOT${clear} capable of backing up your packages/tweaks data.\n"
 printf "I am ${red}not${clear} responsable for any damaged that may be caused to your system.\n"
 printf "Blindly installing every package listed in an accessible and non-encrypted text file is ${red}not a good idea${clear}.\n"
@@ -19,12 +19,13 @@ printf "That being said, type ${green}1${clear} if you are fine with running thi
 
 read -p '> ' agreement
 if [[ "$agreement" != 1 ]]; then
+	echo "🛑 You have not agreed 🛑"
 	exit
 fi
 
-echo "1. Backup your tweaks"
-echo "2. Restore your tweaks"
-echo "Anything else to close out of the script."
+echo "1. 💾 Backup your tweaks"
+echo "2. 🛠 Restore your tweaks"
+echo "🧱 Anything else to close out of the script."
 
 read -p '> ' option
 
@@ -33,18 +34,18 @@ backup='/private/var/mobile/Documents/TweaksBackup.txt'
 if [[ "$option" == 1 ]]; then
 	rm "$destination" 2> /dev/null
 	dpkg -l | grep -v '^rc\|gsc\|cy+' | cut -d ' ' -f 3 | sed 1,5d > "$backup" # https://www.reddit.com/comments/3xuiuk/1/cy7z2qs
-	echo "Your file has been saved at the location $backup (inside of your Documents folder)"
+	echo "✅ Your file has been saved at the location $backup (inside of your Documents folder)."
 else
 	# We need gawk to use the "awk" command
 	if [ $(dpkg-query -W -f='${Status}' gawk 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
-		echo "gawk is not installed on this machine. Proceeding to install."
+		echo "⚠️ gawk is not installed on this machine. Proceeding to install ⚠️"
 		apt-get install gawk;
 	fi
 	if [[ "$option" == 2 ]]; then
 		xargs -a <(awk '! /^ *(#|$)/' "$backup") -r -- apt-get install
 		# apt-get install $(grep -vE "^\s*#" "$backup"  | tr "\n" " ")
 	else
-		echo "Invalid option."
+		echo "⚠️ Invalid option ⚠️"
 	fi
 fi
